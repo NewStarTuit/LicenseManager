8d6da467bb57761d5a623c393fbe32ecbf6df34c
Umidjon Mamajonov <new.star.tuit@gmail.com>
2021-10-29 11:34:39 +0500

    Generate new Keys added

--- LicenseManager/MainWindow.xaml.cs
+++ LicenseManager/MainWindow.xaml.cs
@@ -31,18 +31,19 @@ namespace LicenseManager
             {
                 LicenseOwner = "Umidjon Mamajonov",
                 ExpireDate = (DateTime)date_pick.SelectedDate,
-                Product = "Quick Mariant Maker",
+                Product = "My Product",
                 Version = "1.0.0"
-            };
 
-            license_key.Text = LicenseControlller.GenerateLicense(licenseModel);
+            };
+            string prvKey = private_key.Text;
+            license_key.Text = LicenseControlller.GenerateLicense(licenseModel, prvKey);
         }
 
         private void verify_license_file(object sender, RoutedEventArgs e)
         {
             LicenseModel licenseModel = new LicenseModel();
-
-            if (LicenseControlller.VerifyLicense(out licenseModel))
+            string pubKey = public_key.Text;
+            if (LicenseControlller.VerifyLicense(out licenseModel, pubKey))
             {
                 license_verify.Text = string.Format("{0}\r\n{1}\r\n{2}\r\n{3}\r\n{4}", licenseModel.Status,
                                                                                         licenseModel.Product,
@@ -52,6 +53,14 @@ namespace LicenseManager
             }
             else
                 license_verify.Text = "LICENSE INVALID";
+        }
+
+        private void generate_new_keys(object sender, RoutedEventArgs e)
+        {
+            string[] keys = LicenseControlller.GenerateNewKeys();
+            public_key.Text = keys[0];
+            private_key.Text = keys[1];
+
         }
     }
 }
